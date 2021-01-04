ce2cf6dd8b6775509e1742e7a30da0140e5fdfdc
liaozhongxun <869664233@qq.com>
2021-01-04 22:11:34 +0800

    first

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,25 +1,34 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import Home from '../views/Home.vue'
+import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+import Home from "../views/home/index.vue";
 
 const routes: Array<RouteRecordRaw> = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+    {
+        path: "/",
+        name: "Home",
+        component: Home,
+    },
+    {
+        path: "/mine",
+        name: "mine",
+        // 路由级代码拆分
+        // 这会为此路由生成一个单独的块 (mine.[hash].js)
+        // 在访问路线时被延迟加载
+        component: () =>
+            import(/* webpackChunkName: "mine" */ "../views/mine/index.vue"),
+    },
+    {
+        path: "/profile",
+        name: "Profile",
+        component: () =>
+            import(
+                /* webpackChunkName: "profile" */ "../views/profile/index.vue"
+            ),
+    },
+];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+    history: createWebHistory(process.env.BASE_URL),
+    routes,
+});
 
-export default router
+export default router;
---
+++ src/views/mine/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="mine">mine</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue"; //vue自动不通过插件实现自动提示
+export default defineComponent({
+    data() {
+        return {};
+    },
+});
+</script>
